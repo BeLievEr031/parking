b97299f97a606e8d00413a971d8804bf19a22d15
BeLieVer031 <sandyrajak031@gmail.com>
2025-01-30 14:45:46 +0530

    add bottom sheet

--- parking-app/app/(screens)/slot/[slotid].tsx
+++ parking-app/app/(screens)/slot/[slotid].tsx
@@ -1,22 +1,105 @@
-import { View, Text, SafeAreaView, Image } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, Image, TouchableOpacity, KeyboardAvoidingView, Platform, Keyboard } from 'react-native'
+import React, { useEffect, useMemo, useRef } from 'react'
 import singleSlotStyle from '@/styles/singleSlot';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet';
+import { GestureHandlerRootView, TextInput } from 'react-native-gesture-handler';
 
 const Slot = () => {
+    const sheetRef = useRef<BottomSheet>(null);
+
+    const snapPoints = useMemo(() => ["60%", "75%"], []);
+
+    const handleBottomSheetOpen = () => {
+        sheetRef.current?.snapToIndex(0)
+    }
+
+
+    useEffect(() => {
+        const didKeyBoardOpen = Keyboard.addListener("keyboardDidShow", () => {
+            sheetRef.current?.snapToIndex(1)
+
+        })
+
+        const didKeyBoardClose = Keyboard.addListener("keyboardDidHide", () => {
+            sheetRef.current?.snapToIndex(0)
+        })
+    }, [])
     return (
-        <SafeAreaView>
-            <Image source={require("../../../assets/images/parkingslot.jpg")} style={singleSlotStyle.slotImg} />
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <SafeAreaView style={{ flex: 1 }}>
+                <Image source={require("../../../assets/images/parkingslot.jpg")} style={singleSlotStyle.slotImg} />
+
+                <View style={singleSlotStyle.slotInfo}>
+                    <Text style={singleSlotStyle.parkingSlot}>
+                        Car Parkings
+                    </Text>
+                    <Text style={singleSlotStyle.slotName}>Cinema Park</Text>
+                    <Text style={singleSlotStyle.slotAddress}>Resort Street, Cooper</Text>
+                    <Text style={singleSlotStyle.slotPrice}>25/Hours.</Text>
+
+                    <Text style={singleSlotStyle.descText}>
+                        Description
+                    </Text>
+                    <Text style={singleSlotStyle.description}>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum, praesentium. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat, voluptatem!
+                    </Text>
+                </View>
+
+                <View style={singleSlotStyle.ownerInfoCont}>
+                    <Text style={singleSlotStyle.operatedText}>
+                        Operated By
+                    </Text>
+
+                    <View style={singleSlotStyle.ownerInfo}>
+                        <MaterialCommunityIcons name='steering' size={45} color={"#1a73e8"} />
+
+                        <View>
+                            <Text style={singleSlotStyle.ownerName}>Sandeep Rajak</Text>
+                            <Text style={singleSlotStyle.phone}>9511849241</Text>
+                        </View>
+                    </View>
+
+                </View>
+
+                <TouchableOpacity style={singleSlotStyle.bookSlotBtn}
+                    activeOpacity={0.8}
+                    onPress={handleBottomSheetOpen}
+                >
+                    <Text style={singleSlotStyle.bookSlotText}>Book Slot</Text>
+                </TouchableOpacity>
 
 
-            <View style={singleSlotStyle.slotInfo}>
-                <Text style={singleSlotStyle.parkingSlot}>
-                    Car Parkings
-                </Text>
-                <Text style={singleSlotStyle.slotName}>Cinema Park</Text>
-                <Text style={singleSlotStyle.slotAddress}>Resort Street, Cooper</Text>
-                <Text style={singleSlotStyle.slotPrice}>25/Hours.</Text>
-            </View>
-        </SafeAreaView>
+                <BottomSheet
+                    ref={sheetRef}
+                    snapPoints={snapPoints}
+                    enableDynamicSizing={false}
+                    enablePanDownToClose={true}
+                    index={-1}
+                >
+                    <BottomSheetView style={{ paddingHorizontal: 10 }}>
+                        <KeyboardAvoidingView
+                            behavior={Platform.OS === "ios" ? "padding" : "height"}
+                        >
+                            <Text style={singleSlotStyle.bookSlotBSText}>Book a slot</Text>
+
+                            <TextInput style={singleSlotStyle.BSInput} placeholder='Name' />
+                            <TextInput style={singleSlotStyle.BSInput} placeholder='Car Name' />
+                            <TextInput style={singleSlotStyle.BSInput} placeholder='Car Model' />
+                            <TextInput keyboardType='number-pad' style={singleSlotStyle.BSInput} placeholder='Car Number' />
+
+                            <TouchableOpacity style={singleSlotStyle.bookBtn}>
+                                <Text style={singleSlotStyle.bookBtnText}>
+                                    Book a slot now
+                                </Text>
+                            </TouchableOpacity>
+                        </KeyboardAvoidingView>
+                    </BottomSheetView>
+                </BottomSheet>
+
+            </SafeAreaView>
+        </GestureHandlerRootView>
+
     )
 }
 
--- parking-app/styles/singleSlot.ts
+++ parking-app/styles/singleSlot.ts
@@ -1,3 +1,5 @@
+import { NotoSerif_700Bold } from '@expo-google-fonts/noto-serif';
+import { Lato_700Bold, Lato_400Regular } from '@expo-google-fonts/lato';
 import { StyleSheet } from "react-native"
 
 
@@ -31,7 +33,91 @@ const singleSlotStyle = StyleSheet.create({
         color: "#1a73e8",
         textAlign: "right",
         fontSize: 18
+    },
+    descText: {
+        fontSize: 20,
+        marginVertical: 10,
+        fontFamily: "Lato_700Bold"
+    },
+    description: {
+        fontSize: 14,
+        textAlign: "justify",
+        color: "gray",
+        fontFamily: "Lato_400Regular"
+    },
+    ownerInfoCont: {
+        width: "100%",
+        marginTop: 10,
+        paddingHorizontal: 15,
+    },
+
+    operatedText: {
+        fontSize: 20,
+        fontFamily: "Lato_700Bold",
+    },
+
+    ownerInfo: {
+        width: "100%",
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 5,
+        marginTop: 15
+    },
+
+    ownerName: {
+        fontSize: 15,
+        fontFamily: "NotoSerif_700Bold"
+    },
+
+    phone: {
+        color: "gray",
+        fontWeight: "bold"
+    },
+
+    bookSlotBtn: {
+        backgroundColor: "#1a73e8",
+        padding: 15,
+        width: "90%",
+        alignSelf: "center",
+        borderRadius: 10,
+        marginTop: 30
+    },
+    bookSlotText: {
+        color: "white",
+        fontFamily: "NotoSerif_700Bold",
+        fontSize: 18,
+        textAlign: "center"
+    },
+
+    bookSlotBSText: {
+        fontSize: 25,
+        fontFamily: "NotoSerif_700Bold",
+        marginTop: 10,
+        marginBottom: 15,
+        textAlign: "center"
+    },
+
+    BSInput: {
+        borderWidth: 1,
+        borderColor: "black",
+        marginTop: 10,
+        borderRadius: 10,
+        height: 50
+    },
+    bookBtn: {
+        backgroundColor: "#1a73e8",
+        marginTop: 10,
+        paddingHorizontal: 10,
+        paddingVertical: 15,
+        borderRadius: 10
+    },
+    bookBtnText: {
+        fontSize: 18,
+        fontFamily: "Lato_700Bold",
+        color: "white",
+        textAlign: "center"
     }
+
 })
 
 export default singleSlotStyle;
